f4c3a0762208167e033fdbea6b44b38450d38789
Will Hawker <will.hawker@bbc.co.uk>
2017-06-12 21:19:12 +0100

    Add first attempt at testing context created by BaseChart component

--- test/components/BaseChart/BaseChart.spec.js
+++ test/components/BaseChart/BaseChart.spec.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import { mount } from 'enzyme';
 import Highcharts from 'highstock-release';
 import BaseChart from '../../../src/components/BaseChart';
 import { createMockChart } from '../../test-utils';
 
+class ChartContextReceiver extends PureComponent {
+  static contextTypes = {
+    chart: PropTypes.object
+  };
+
+  render () {
+    return (
+      <div />
+    );
+  }
+}
+
 describe('<BaseChart />', function ()  {
   describe('on mount', function () {
     let clock;
     let sandbox;
+    let chart;
 
     beforeEach(function () {
+      chart = createMockChart();
       sandbox = sinon.sandbox.create();
-      sandbox.stub(Highcharts, 'chart').returns(createMockChart());
-      sandbox.stub(Highcharts, 'stockChart').returns(createMockChart());
+      sandbox.stub(Highcharts, 'chart').returns(chart);
+      sandbox.stub(Highcharts, 'stockChart').returns(chart);
       clock = sinon.useFakeTimers();
     });
 
@@ -21,18 +36,44 @@ describe('<BaseChart />', function ()  {
       clock.restore();
     });
 
-    it('should create a Highcharts chart when chartType is chart', function () {
-      const wrapper = mount(<BaseChart chartType="chart"/>);
-      clock.tick(1);
-      expect(Highcharts.chart).to.have.been.calledWith(wrapper.node.domNode);
-      expect(Highcharts.stockChart).not.to.have.been.called;
+    describe('chartType = chart', function () {
+      it('should create a Highcharts chart', function () {
+        const wrapper = mount(<BaseChart chartType="chart"/>);
+        clock.tick(1);
+        expect(Highcharts.chart).to.have.been.calledWith(wrapper.node.domNode);
+        expect(Highcharts.stockChart).not.to.have.been.called;
+      });
+
+      it('should create a chart context', function () {
+        const wrapper = mount(
+          <BaseChart chartType="chart">
+            <ChartContextReceiver />
+          </BaseChart>
+        );
+        clock.tick(1);
+        const child = wrapper.find(ChartContextReceiver).getNode();
+        expect(child.context.chart).to.eql(chart);
+      });
     });
 
-    it('should create a Highstocks chart when chartType is stockChart', function () {
-      const wrapper = mount(<BaseChart chartType="stockChart"/>);
-      clock.tick(1);
-      expect(Highcharts.stockChart).to.have.been.calledWith(wrapper.node.domNode);
-      expect(Highcharts.chart).not.to.have.been.called;
+    describe('chartType = stockChart', function () {
+      it('should create a Highstocks chart', function () {
+        const wrapper = mount(<BaseChart chartType="stockChart"/>);
+        clock.tick(1);
+        expect(Highcharts.stockChart).to.have.been.calledWith(wrapper.node.domNode);
+        expect(Highcharts.chart).not.to.have.been.called;
+      });
+
+      it('should create a chart context', function () {
+        const wrapper = mount(
+          <BaseChart chartType="stockChart">
+            <ChartContextReceiver />
+          </BaseChart>
+        );
+        clock.tick(1);
+        const child = wrapper.find(ChartContextReceiver).getNode();
+        expect(child.context.chart).to.eql(chart);
+      });
     });
   });
 });
