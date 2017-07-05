43dcf5280ca9d776a44d9ae332e6ef2aca3a3c3d
William Hawker <william.hawker@i2owater.com>
2017-07-05 12:52:36 +0100

    getChartType is a provided method

--- src/components/ChartProvider/index.js
+++ src/components/ChartProvider/index.js
@@ -21,7 +21,7 @@ export default function provideChart(WrappedComponent) {
 
       providedProps(
         'ChartProvider',
-        ['get', 'update', 'addAxis', 'addSeries', 'setTitle', 'getChart']
+        ['get', 'update', 'addAxis', 'addSeries', 'setTitle', 'getChart', 'getChartType']
       );
     }
 
