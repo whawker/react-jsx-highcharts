cb90016d876367b423a3fec52e6aaf94218a3c47
William Hawker <william.hawker@i2owater.com>
2017-07-21 14:02:20 +0100

    Add inverted chart example

---
+++ examples/InvertedChart/exampleCode.js
@@ -0,0 +1,45 @@
+export default `
+render () {
+  const plotOptions = {
+    areaspline: {
+      fillOpacity: 1,
+      lineWidth: 0,
+      marker: {
+        enabled: false,
+        states: {
+          hover: { enabled: false }
+        }
+      }
+    }
+  };
+
+  const positioner = (w, h, point) => ({x: 0, y: point.plotY + (h / 2) + 8});
+    
+  const { seasons, totalGoals, avgGoalsPerGame } = this.state; 
+
+  return (
+    <div className="app">
+      <HighchartsChart plotOptions={plotOptions}>
+        <Chart inverted marginLeft={300} height={650} />
+
+        <Title>Premier League Goal Stats: 1992-93 to 2016-17</Title>
+
+        <Subtitle>Source: myfootballfacts.com</Subtitle>
+
+        <XAxis type="category" categories={seasons} crosshair={{ zIndex: 10 }} lineWidth={0} tickLength={0}>
+          <XAxis.Title>Season</XAxis.Title>
+        </XAxis>
+
+        <YAxis id="total" min={800} gridLineWidth={0} labels={{ enabled: false }}>
+          <AreaSplineSeries id="totalGoals" name="Total Goals" data={totalGoals} color="#38003c" />
+        </YAxis>
+
+        <YAxis id="average" min={2.4} max={5} gridLineWidth={0} labels={{ enabled: false }}>
+          <AreaSplineSeries id="avgGoalsPerGame" name="Average Goals per Game" data={avgGoalsPerGame} color="#e90052" />
+        </YAxis>
+
+        <Tooltip shared positioner={positioner} backgroundColor="transparent" borderWidth={0} shadow={false} />
+      </HighchartsChart>
+    </div>
+  );
+}`;
