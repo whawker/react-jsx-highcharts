2820c4a221d32c42d0d4fe40e7256ba2a306118c
Will Hawker <will.hawker@bbc.co.uk>
2017-08-12 19:14:05 +0100

    Standardise code formatting in new examples

--- examples/Treemap/App.js
+++ examples/Treemap/App.js
@@ -8,53 +8,54 @@ import code from './exampleCode';
 class App extends Component {
 
   render() {
-
     const treemapData = [{
-            name: 'A',
-            value: 6,
-            colorValue: 1
-        }, {
-            name: 'B',
-            value: 6,
-            colorValue: 2
-        }, {
-            name: 'C',
-            value: 4,
-            colorValue: 3
-        }, {
-            name: 'D',
-            value: 3,
-            colorValue: 4
-        }, {
-            name: 'E',
-            value: 2,
-            colorValue: 5
-        }, {
-            name: 'F',
-            value: 2,
-            colorValue: 6
-        }, {
-            name: 'G',
-            value: 1,
-            colorValue: 7
-        }]
+      name: 'A',
+      value: 6,
+      colorValue: 1
+    }, {
+      name: 'B',
+      value: 6,
+      colorValue: 2
+    }, {
+      name: 'C',
+      value: 4,
+      colorValue: 3
+    }, {
+      name: 'D',
+      value: 3,
+      colorValue: 4
+    }, {
+      name: 'E',
+      value: 2,
+      colorValue: 5
+    }, {
+      name: 'F',
+      value: 2,
+      colorValue: 6
+    }, {
+      name: 'G',
+      value: 1,
+      colorValue: 7
+    }];
 
     const colorAxis = {
-            minColor: '#FFFFFF',
-            maxColor: Highcharts.getOptions().colors[0]
-        };
-    
+      minColor: '#FFFFFF',
+      maxColor: Highcharts.getOptions().colors[0]
+    };
+
 
     return (
       <div className="app">
         <HighchartsChart colorAxis={colorAxis}>
           <Title>Treemap</Title>
-            <Legend/>
-            <XAxis id="name"/>
-            <YAxis id="value">
-                <TreemapSeries id='tree' name='Tree' data={treemapData} layoutAlgorithm='squarified'/>
-            </YAxis>
-        
+
+          <Legend />
+
+          <XAxis />
+
+          <YAxis id="value">
+            <TreemapSeries id="tree" name="Tree" data={treemapData} layoutAlgorithm="squarified" />
+          </YAxis>
         </HighchartsChart>
 
         <ExampleCode name="Treemap">{code}</ExampleCode>
--- examples/TreemapDrilldown/exampleCode.js
+++ examples/TreemapDrilldown/exampleCode.js
@@ -1,37 +1,36 @@
 export default `
   render() {
-        var levels = [{
-            level: 1,
-            dataLabels: {
-                enabled: true
-            },
-            borderWidth: 3
-        }];
-        var dataLabels = {
-            enabled: false
-        };
-        var tooltipText = function (e) {
-            console.log(e, this);
-            return \`\${this.key}: \${this.point.value}\`;
-        }
-
-
-        return (
-            <div className="app">
-                <HighchartsChart>
-                    <Title>Treemap</Title>
-                    <XAxis />
-                    <YAxis id="value">
-                        <TreemapSeries data={points} layoutAlgorithm='squarified' allowDrillToNode={true} animationLimit={1000} dataLabels={dataLabels} levelIsConstant={false} levels={levels} />
-                    </YAxis>
-                    <Tooltip
-                        formatter={tooltipText}
-                    />
-
-                </HighchartsChart>
-
-                <ExampleCode name="Treemap">{code}</ExampleCode>
-            </div>
-        );
+    var levels = [{
+      level: 1,
+      dataLabels: {
+        enabled: true
+      },
+      borderWidth: 3
+    }];
+    var dataLabels = {
+      enabled: false
+    };
+    var tooltipText = function (e) {
+      return \`\${this.key}: \${this.point.value}\`;
     }
+
+
+    return (
+      <div className="app">
+        <HighchartsChart>
+          <Title>Treemap with Drilldown</Title>
+
+          <XAxis />
+
+          <YAxis id="value">
+            <TreemapSeries data={points} layoutAlgorithm="squarified" allowDrillToNode animationLimit={1000} dataLabels={dataLabels} levelIsConstant={false} levels={levels} />
+          </YAxis>
+
+          <Tooltip formatter={tooltipText} />
+        </HighchartsChart>
+
+        <ExampleCode name="Treemap">{code}</ExampleCode>
+      </div>
+    );
+  }
 }`;
