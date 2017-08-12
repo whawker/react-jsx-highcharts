e7ef5b69568ab6b25790c7df4e99ab61ba5fdf96
Will Hawker <will.hawker@bbc.co.uk>
2017-08-12 19:21:32 +0100

    Move Treemap data into component state

--- examples/Treemap/exampleCode.js
+++ examples/Treemap/exampleCode.js
@@ -1,58 +1,43 @@
-export default `
+export default `const colorAxis = {
+  minColor: '#FFFFFF',
+  maxColor: Highcharts.getOptions().colors[0]
+};
+
+class App extends Component {
+
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      treemapData: [
+        { name: 'A', value: 6, colorValue: 1 },
+        { name: 'B', value: 6, colorValue: 2 },
+        { name: 'C', value: 4, colorValue: 3 },
+        { name: 'D', value: 3, colorValue: 4 },
+        { name: 'E', value: 2, colorValue: 5 },
+        { name: 'F', value: 2, colorValue: 6 },
+        { name: 'G', value: 1, colorValue: 7 }
+      ]
+    }
+  }
+
   render() {
+    const treemapData = this.state.treemapData;
 
-    const treemapData = [{
-      name: 'A',
-      value: 6,
-      colorValue: 1
-    }, {
-      name: 'B',
-      value: 6,
-      colorValue: 2
-    }, {
-      name: 'C',
-      value: 4,
-      colorValue: 3
-    }, {
-      name: 'D',
-      value: 3,
-      colorValue: 4
-    }, {
-      name: 'E',
-      value: 2,
-      colorValue: 5
-    }, {
-      name: 'F',
-      value: 2,
-      colorValue: 6
-    }, {
-      name: 'G',
-      value: 1,
-      colorValue: 7
-    }];
+    return (
+      <div className="app">
+        <HighchartsChart colorAxis={colorAxis}>
+          <Title>Treemap</Title>
 
-    const colorAxis = {
-      minColor: '#FFFFFF',
-      maxColor: Highcharts.getOptions().colors[0]
-    };
-  
+          <Legend />
 
-  return (
-    <div className="app">
-      <HighchartsChart colorAxis={colorAxis}>
-        <Title>Treemap</Title>
+          <XAxis />
 
-        <Legend />
-
-        <XAxis />
-
-        <YAxis id="value">
-          <TreemapSeries id="tree" name="Tree" data={treemapData} layoutAlgorithm="squarified" />
-        </YAxis>
-      
-      </HighchartsChart>
-  
-      <ExampleCode name="Treemap">{code}</ExampleCode>
-    </div>
-  );
+          <YAxis id="value">
+            <TreemapSeries id="tree" name="Tree" data={treemapData} layoutAlgorithm="squarified" />
+          </YAxis>
+        </HighchartsChart>
+      </div>
+    );
+  }
 }`;
