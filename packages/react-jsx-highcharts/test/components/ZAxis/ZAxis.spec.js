dfe1146d5fefa329f02fb70d5cc33274afc1c264
William Hawker <william.hawker@i2owater.com>
2017-08-16 10:47:34 +0100

    Add tests for new components

--- packages/react-jsx-highcharts/test/components/ZAxis/ZAxis.spec.js
+++ packages/react-jsx-highcharts/test/components/ZAxis/ZAxis.spec.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import YAxis from '../../../src/components/YAxis/YAxis';
+import ZAxis from '../../../src/components/ZAxis/ZAxis';
 import Axis from '../../../src/components/Axis';
 
-describe('<YAxis />', function ()  {
+describe('<ZAxis />', function ()  {
   it('renders an <Axis />', function ()  {
-    const wrapper = shallow(<YAxis id="y" />);
+    const wrapper = shallow(<ZAxis />);
     expect(wrapper).to.have.type(Axis);
   });
 
-  it('renders an <Axis dimension="y" />', function () {
-    const wrapper = shallow(<YAxis id="yAxis" />);
-    expect(wrapper).to.have.prop('dimension').equal('y');
+  it('should always have the id `zAxis`', function ()  {
+    const wrapper = shallow(<ZAxis id="customId" />);
+    expect(wrapper).to.have.prop('id').equal('zAxis');
+  });
+
+  it('should NOT be a dynamic axis', function ()  {
+    const wrapper = shallow(<ZAxis />);
+    expect(wrapper).to.have.prop('dynamicAxis').equal(false);
+  });
+
+  it('renders an <Axis dimension="z" />', function () {
+    const wrapper = shallow(<ZAxis id="ZAxis" />);
+    expect(wrapper).to.have.prop('dimension').equal('z');
   });
 
   it('passes other props through to <Axis />', function () {
-    const wrapper = shallow(<YAxis id="myOtherAxis" tickLength={1337} />);
+    const wrapper = shallow(<ZAxis tickLength={1337} />);
     expect(wrapper).to.have.prop('tickLength').equal(1337);
   });
 });
