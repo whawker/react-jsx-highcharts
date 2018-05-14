773825e61321cbf2d0786998220b296c73ae73ec
Will Hawker <w.d.hawker@gmail.com>
2018-05-14 12:15:42 +0100

    Add missing axis methods for adding/removing plot lines

--- packages/react-jsx-highcharts/src/components/AxisProvider/index.js
+++ packages/react-jsx-highcharts/src/components/AxisProvider/index.js
@@ -32,6 +32,8 @@ export default function provideAxis(Component) {
             remove: axis.remove.bind(axis),
             addPlotBand: clean(axis.addPlotBand.bind(axis)),
             removePlotBand: axis.removePlotBand.bind(axis),
+            addPlotLine: clean(axis.addPlotLine.bind(axis)),
+            removePlotLine: axis.removePlotLine.bind(axis),
             getExtremes: axis.getExtremes.bind(axis),
             setExtremes: axis.setExtremes.bind(axis),
             setTitle: clean(axis.setTitle.bind(axis))
