276a966513c25893dedfb69098c62bc2ad8cdd85
Will Hawker <w.d.hawker@gmail.com>
2018-05-24 14:22:52 +0100

    Add tests for series that do not require an axis

--- packages/react-jsx-highcharts/test/components/PieSeries/PieSeries.spec.js
+++ packages/react-jsx-highcharts/test/components/PieSeries/PieSeries.spec.js
@@ -13,6 +13,11 @@ describe('<PieSeries />', function ()  {
     expect(wrapper).to.have.prop('type').equal('pie');
   });
 
+  it('does not require an axis', function () {
+    const wrapper = shallow(<PieSeries id="mySeries" />);
+    expect(wrapper).to.have.prop('requiresAxis').equal(false);
+  });
+
   it('passes other props through to <Series />', function () {
     const wrapper = shallow(<PieSeries id="myOtherSeries" data={[1, 2, 3, 4]} />);
     expect(wrapper).to.have.prop('data').eql([1, 2, 3, 4]);
