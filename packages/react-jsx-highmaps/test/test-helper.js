323424b498751aa7ba58c0c70bc53a1b63815913
Will Hawker <w.d.hawker@gmail.com>
2018-10-13 16:14:35 +0100

    Add component tests

---
+++ packages/react-jsx-highmaps/test/test-helper.js
@@ -0,0 +1,40 @@
+import { JSDOM } from 'jsdom';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+chai.use(sinonChai);
+
+function copyProps(src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter(prop => typeof target[prop] === 'undefined')
+    .map(prop => Object.getOwnPropertyDescriptor(src, prop));
+  Object.defineProperties(target, props);
+}
+
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+window.Date = global.Date;
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js'
+};
+global.expect = expect;
+global.sinon = sinon;
+
+var Enzyme = require('enzyme');
+var Adapter = require('enzyme-adapter-react-16');
+Enzyme.configure({ adapter: new Adapter() });
+global.shallow = Enzyme.shallow;
+global.mount = Enzyme.mount;
+copyProps(window, global);
+
+// this has to happen after the globals are set up because `chai-enzyme`
+// will require `enzyme`, which requires `react`, which ultimately
+// requires `fbjs/lib/ExecutionEnvironment` which (at require time) will
+// attempt to determine the current environment (this is where it checks
+// for whether the globals are present). Hence, the globals need to be
+// initialized before requiring `chai-enzyme`.
+chai.use(require('chai-enzyme')());
---
+++ packages/react-jsx-highmaps/test/test-utils.js
@@ -0,0 +1,82 @@
+import sinon from 'sinon';
+
+export const uuidRegex = /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
+
+const noop = () => {};
+export const Highcharts = {
+  mapChart: noop,
+  addEvent: noop,
+  removeEvent: noop,
+  maps: {
+    'mock/map': { some: 'data' }
+  }
+};
+
+export const createMockChart = () => ({
+  addAxis: sinon.stub(),
+  addSeries: sinon.stub(),
+  get: sinon.stub(),
+  update: sinon.stub(),
+  setTitle: sinon.stub(),
+  destroy: sinon.stub(),
+  showLoading: sinon.stub(),
+  hideLoading: sinon.stub(),
+  addCredits: sinon.stub(),
+})
+
+export const createMockProvidedChart = () => {
+  const chartStubs = createMockChart();
+
+  return {
+    chartStubs,
+    getChart: () => chartStubs
+  }
+};
+
+export const createMockAxis = ({ ...additional }) => ({
+  ...additional,
+  remove: sinon.stub(),
+  addPlotBand: sinon.stub(),
+  removePlotBand: sinon.stub(),
+  addPlotLine: sinon.stub(),
+  removePlotLine: sinon.stub(),
+  getExtremes: sinon.stub(),
+  setExtremes: sinon.stub(),
+  update: sinon.stub(),
+  setTitle: sinon.stub()
+});
+
+export const createMockProvidedAxis = ({ object, ...additional }) => {
+  const axisStubs = createMockAxis(additional);
+
+  return {
+    axisStubs,
+    getAxis: () => ({
+      object,
+      ...additional,
+      ...axisStubs
+    })
+  }
+};
+
+export const createMockSeries = ({ ...additional }) => ({
+  ...additional,
+  remove: sinon.stub(),
+  setData: sinon.stub(),
+  setVisible: sinon.stub(),
+  update: sinon.stub()
+});
+
+export const createMockProvidedSeries = ({ object, ...additional }) => {
+  const seriesStubs = createMockSeries(additional);
+
+  return {
+    seriesStubs,
+    getSeries: () => ({
+      object,
+      ...additional,
+      ...seriesStubs
+    })
+  }
+};
+
