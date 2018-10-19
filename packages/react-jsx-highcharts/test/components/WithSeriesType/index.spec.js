da7b10440be8a1feddb1c2e5e7b8f69a9a708b66
Will Hawker <w.d.hawker@gmail.com>
2018-10-19 08:56:01 +0100

    Ensure created series component allows passing through props

--- packages/react-jsx-highcharts/test/components/WithSeriesType/index.spec.js
+++ packages/react-jsx-highcharts/test/components/WithSeriesType/index.spec.js
@@ -8,21 +8,30 @@ describe('withSeriesType', function ()  {
     const SeriesComponent = withSeriesType('line');
     const wrapper = shallow(<SeriesComponent />);
     expect(wrapper).to.have.type(Series);
-
   });
+
   it(`should set type attribute <Series type="line" />`, function () {
     const SeriesComponent = withSeriesType('line');
     const wrapper = shallow(<SeriesComponent />);
     expect(wrapper).to.have.prop('type').equal('line');
   });
+
+  it(`the created component should pass additional props through to Series`, function () {
+    const SeriesComponent = withSeriesType('line');
+    const wrapper = shallow(<SeriesComponent data={[1, 2, 3, 4]} />);
+    expect(wrapper).to.have.prop('data').eql([1, 2, 3, 4]);
+  });
+
   it(`should set id propType`, function () {
     const SeriesComponent = withSeriesType('line');
     expect(SeriesComponent.propTypes).to.have.property('id');
   });
+
   it(`should add additionalPropTypes`, function () {
     const SeriesComponent = withSeriesType('line', {}, { baseSeries: PropTypes.string.isRequired });
     expect(SeriesComponent.propTypes).to.have.property('baseSeries');
   });
+
   it(`should pass additionalProps to Series`, function () {
     const SeriesComponent = withSeriesType('line', { requiresAxis: false });
     const wrapper = shallow(<SeriesComponent />);
