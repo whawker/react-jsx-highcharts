e7a0756f5e40064859d7c95518523beffe6c1562
Jørund Vier Skriubakken <joerund.vier.skriubakken@esmartsystems.com>
2018-10-31 15:29:38 +0100

    highcharts-gantt: exported some common modules from the Highcharts package (not quite sure how to figure out which are remaining)

--- packages/react-jsx-highcharts-gantt/src/index.js
+++ packages/react-jsx-highcharts-gantt/src/index.js
@@ -1,1 +1,24 @@
-// TODO: Is it here that the "common modules should be exported (Title, Tooltip, etc)" ?
+import { withSeriesType } from 'react-jsx-highcharts';
+
+// TODO: Add more common modules that Highcarts Gantt support
+export {
+  Chart,
+  Title,
+  Subtitle,
+  Tooltip,
+} from 'react-jsx-highcharts';
+
+
+// Charts
+export { default as HighchartsGanttChart } from './components/HighchartsGanttChart';
+
+// Graph Parts
+
+export { default as XAxis } from './components/XAxis'; // TODO: ?
+export { default as YAxis } from './components/YAxis'; // TODO: ?
+
+//TODO: create and export Pathfinder component
+//export { default as Pathfinder } from './components/Pathfinder';
+
+// Series
+export const GanttSeries = withSeriesType('gantt');
