bfc24a46f0ad43025019b6843930592324e48a1d
Jørund Vier Skriubakken <joerund.vier.skriubakken@esmartsystems.com>
2018-11-01 09:40:02 +0100

    highcharts-gantt: exposed component Marker as Pathfinder.Marker

--- packages/react-jsx-highcharts-gantt/src/components/Pathfinder/index.js
+++ packages/react-jsx-highcharts-gantt/src/components/Pathfinder/index.js
@@ -1,4 +1,6 @@
 import Pathfinder from './Pathfinder';
 import { provideChart } from 'react-jsx-highcharts';
+import Marker from "./Marker";
 const ChartPathfinder = provideChart(Pathfinder);
+ChartPathfinder.Marker = provideChart(Marker);
 export default ChartPathfinder;
