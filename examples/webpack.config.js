d3ecdfca55017919be574ac9a0bed9e0e42c60fa
Will Hawker <w.d.hawker@gmail.com>
2018-11-01 11:47:31 +0000

    Comment out examples that do not need to be built right now

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -53,10 +53,11 @@ const ganttExamples = [
 ]
 
 const examples = [].concat(
-  highchartsExamples.map(({ name, additional = [] }) => ({ name, additional: ['highcharts', 'react-jsx-highcharts'].concat(additional) })),
-  highchartsStyledExamples.map(({ name, additional = [] }) => ({ name, additional: ['highcharts/js/highcharts', 'react-jsx-highcharts'].concat(additional) })),
-  highstockExamples.map(({ name, additional = [] }) => ({ name, additional: ['highcharts/highstock', 'react-jsx-highstock'].concat(additional) })),
-  highmapsExamples.map(({ name, additional = [] }) => ({ name, additional: ['highcharts/highmaps', 'react-jsx-highmaps'].concat(additional) })),
+  // TODO: Uncomment these before merging to master
+  // highchartsExamples.map(({ name, additional = [] }) => ({ name, additional: ['highcharts', 'react-jsx-highcharts'].concat(additional) })),
+  // highchartsStyledExamples.map(({ name, additional = [] }) => ({ name, additional: ['highcharts/js/highcharts', 'react-jsx-highcharts'].concat(additional) })),
+  // highstockExamples.map(({ name, additional = [] }) => ({ name, additional: ['highcharts/highstock', 'react-jsx-highstock'].concat(additional) })),
+  // highmapsExamples.map(({ name, additional = [] }) => ({ name, additional: ['highcharts/highmaps', 'react-jsx-highmaps'].concat(additional) })),
   ganttExamples.map(({ name, additional = [] }) => ({ name, additional: ['highcharts/highcharts-gantt', 'react-jsx-highcharts-gantt'].concat(additional) }))
 );
 
