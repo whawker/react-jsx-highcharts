16e9d1782642858232228b84ca0b8d4d92c6e286
Jørund Vier Skriubakken <joerund.vier.skriubakken@esmartsystems.com>
2018-11-01 09:57:55 +0100

    highcharts-gantt: Added lifecycle methods to Pathfinder component

--- packages/react-jsx-highcharts-gantt/src/components/Pathfinder/Pathfinder.js
+++ packages/react-jsx-highcharts-gantt/src/components/Pathfinder/Pathfinder.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { attempt } from 'lodash-es';
+import {getModifiedProps} from 'react-jsx-highcharts';
 
 class Pathfinder extends Component {
 
@@ -20,6 +22,31 @@ class Pathfinder extends Component {
     this.state = {
       rendered: false
     };
+  }
+
+  componentDidMount () {
+    const { children, ...rest } = this.props;
+    this.updatePathfinder({
+      ...rest
+    });
+  }
+
+  componentDidUpdate (prevProps) {
+    const modifiedProps = getModifiedProps(prevProps, this.props);
+    if (modifiedProps !== false) {
+      this.updatePathfinder(modifiedProps);
+    }
+  }
+
+  componentWillUnmount () {
+    attempt(this.updatePathfinder, { enabled: false });
+  }
+
+  updatePathfinder = config => {
+    const chart = this.props.getChart();
+    chart.update({
+      pathfinder: config
+    }, true);
   }
 
   render () {
