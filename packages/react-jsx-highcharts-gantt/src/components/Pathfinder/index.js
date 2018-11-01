fa45d7ab76f64b3375e066205c6c16fc38e96683
Jørund Vier Skriubakken <joerund.vier.skriubakken@esmartsystems.com>
2018-11-01 08:53:03 +0100

    highcharts-gantt: Introduced Marker component

--- packages/react-jsx-highcharts-gantt/src/components/Pathfinder/index.js
+++ packages/react-jsx-highcharts-gantt/src/components/Pathfinder/index.js
@@ -1,4 +1,2 @@
 import Pathfinder from './Pathfinder';
 export default Pathfinder;
-
-// TODO: Does Pathfinder need sub components for the types of the properties: startMarker, endMarker and marker?
