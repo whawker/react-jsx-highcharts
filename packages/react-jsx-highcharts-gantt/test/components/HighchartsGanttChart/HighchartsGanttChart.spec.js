36ad0bebb3f44cdfac8fb4f4a49084e0af66d26c
Jørund Vier Skriubakken <joerund.vier.skriubakken@esmartsystems.com>
2018-11-01 09:47:03 +0100

    highcharts-gantt: added standard tests to HighchartsGanttChart.spec.js

--- packages/react-jsx-highcharts-gantt/test/components/HighchartsGanttChart/HighchartsGanttChart.spec.js
+++ packages/react-jsx-highcharts-gantt/test/components/HighchartsGanttChart/HighchartsGanttChart.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BaseChart } from 'react-jsx-highcharts';
-import { Highcharts } from '../../test-utils';
+import {BaseChart} from 'react-jsx-highcharts';
+import {Highcharts} from '../../test-utils';
 import HighchartsGanttChart from '../../../src/components/HighchartsGanttChart/HighchartsGanttChart';
 
 describe('<HighchartsGanttChart />', () => {
@@ -12,8 +12,23 @@ describe('<HighchartsGanttChart />', () => {
   });
 
   it('renders a <BaseChart />', () => {
-    const wrapper = shallow(<HighchartsGanttChart getHighcharts={testContext.getHighcharts} />);
+    const wrapper = shallow(<HighchartsGanttChart getHighcharts={testContext.getHighcharts}/>);
     expect(wrapper.type()).toEqual(BaseChart);
+  });
+
+  it('renders a <BaseChart /> with the correct creation function', () => {
+    const wrapper = shallow(<HighchartsGanttChart getHighcharts={testContext.getHighcharts}/>);
+    expect(wrapper).toHaveProp('chartCreationFunc', Highcharts.ganttChart);
+  });
+
+  it('renders a <BaseChart /> with the correct chart type', () => {
+    const wrapper = shallow(<HighchartsGanttChart getHighcharts={testContext.getHighcharts}/>);
+    expect(wrapper).toHaveProp('chartType', 'ganttChart');
+  });
+
+  it('passes other props through to <BaseChart />', () => {
+    const wrapper = shallow(<HighchartsGanttChart getHighcharts={testContext.getHighcharts} plotOptions={{c: 'd'}}/>);
+    expect(wrapper).toHaveProp('plotOptions', {c: 'd'});
   });
 
 });
