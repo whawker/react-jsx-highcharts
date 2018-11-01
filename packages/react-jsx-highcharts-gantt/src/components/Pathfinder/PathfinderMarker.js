f239ef6d51df4daa7cce59e6ffb5f17d737adccf
Jørund Vier Skriubakken <joerund.vier.skriubakken@esmartsystems.com>
2018-11-01 12:04:02 +0100

    highcharts-gantt: Pathfinder: Refactored Marker

---
+++ packages/react-jsx-highcharts-gantt/src/components/Pathfinder/PathfinderMarker.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { attempt } from 'lodash-es';
+import { getModifiedProps } from 'react-jsx-highcharts';
+
+class PathfinderMarker extends Component {
+
+  static propTypes = {
+    getChart: PropTypes.func.isRequired, // Provided by ChartProvider
+    enabled: PropTypes.bool.isRequired
+  };
+
+  static defaultProps = {
+    enabled: true,
+    markerType: 'marker'
+  };
+
+  componentDidMount () {
+    const { children, ...rest } = this.props;
+    this.updatePathfinderMarker({
+      ...rest
+    });
+  }
+
+  componentDidUpdate (prevProps) {
+    const modifiedProps = getModifiedProps(prevProps, this.props);
+    if (modifiedProps !== false) {
+      this.updatePathfinderMarker(modifiedProps);
+    }
+  }
+
+  componentWillUnmount () {
+    attempt(this.updatePathfinderMarker, { enabled: false });
+  }
+
+  updatePathfinderMarker = config => {
+    const { getChart, markerType } = this.props
+    const chart = getChart();
+    chart.update({
+      pathfinder: {
+        [markerType]: config
+      }
+    }, true);
+  }
+
+  render () {
+    return null
+  }
+}
+
+export default PathfinderMarker;
---
+++ packages/react-jsx-highcharts-gantt/src/components/Pathfinder/PathfinderStartMarker.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import PathfinderMarker from "./PathfinderMarker";
+
+const PathfinderStartMarker = props => (
+  <PathfinderMarker symbol='diamond' {...props} markerType='startMarker' />
+)
+export  default PathfinderStartMarker
