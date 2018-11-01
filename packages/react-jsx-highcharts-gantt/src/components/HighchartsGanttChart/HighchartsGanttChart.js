b2031abde2fbc252aa29d589e78fba2e64ba4b0e
Jørund Vier Skriubakken <joerund.vier.skriubakken@esmartsystems.com>
2018-11-01 09:41:20 +0100

    highcharts-gantt: added missing import of BaseChart in HighchartsGanttChart

--- packages/react-jsx-highcharts-gantt/src/components/HighchartsGanttChart/HighchartsGanttChart.js
+++ packages/react-jsx-highcharts-gantt/src/components/HighchartsGanttChart/HighchartsGanttChart.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import { BaseChart } from 'react-jsx-highcharts';
 
 class HighchartsGanttChart extends Component {
   static propTypes = {
