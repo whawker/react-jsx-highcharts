2bdd50b12ad8039eee45112d33f6aee33e0b78f6
Jørund Vier Skriubakken <joerund.vier.skriubakken@esmartsystems.com>
2018-11-07 10:57:43 +0100

    highcharts-gantt: Added more examples

---
+++ examples/HighchartsGanttInvertedChart/App.js
@@ -0,0 +1,86 @@
+import React, {Component} from 'react';
+import {
+  GanttSeries,
+  HighchartsGanttChart,
+  Subtitle,
+  Title,
+  Tooltip,
+  withHighcharts,
+  XAxis,
+  YAxis
+} from 'react-jsx-highcharts-gantt';
+import ExampleCode from '../utils/ExampleCode';
+import code from './exampleCode';
+
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <div className="app">
+        <HighchartsGanttChart
+          inverted={true}
+          plotBorderWidth={1}
+        >
+          <Title>Inverted Gantt Chart</Title>
+          <XAxis>
+            {
+              [
+                {
+                  opposite: false
+                },
+                {
+                  opposite: false
+                }
+              ]
+            }
+          </XAxis>
+          <YAxis
+            uniqueNames={true}
+            opposite={true}
+          />
+          <Tooltip xDateFormat={"%e %b %Y, %H:%M"}/>
+          <GanttSeries
+            name={"Project 1"}
+            data={
+              [{
+                start: Date.UTC(2018, 11, 1),
+                end: Date.UTC(2018, 11, 2),
+                completed: 0.95,
+                name: 'Prototyping'
+              }, {
+                start: Date.UTC(2018, 11, 2),
+                end: Date.UTC(2018, 11, 5),
+                completed: 0.5,
+                name: 'Development'
+              }, {
+                start: Date.UTC(2018, 11, 8),
+                end: Date.UTC(2018, 11, 9),
+                completed: 0.15,
+                name: 'Testing'
+              }, {
+                start: Date.UTC(2018, 11, 9),
+                end: Date.UTC(2018, 11, 19),
+                completed: {
+                  amount: 0.3,
+                  fill: '#fa0'
+                },
+                name: 'Development'
+              }, {
+                start: Date.UTC(2018, 11, 10),
+                end: Date.UTC(2018, 11, 23),
+                name: 'Testing'
+              }
+              ]
+            }
+          />
+        </HighchartsGanttChart>
+
+        <ExampleCode name="Gantt">{code}</ExampleCode>
+      </div>
+    );
+  }
+}
+
+export default withHighcharts(App, Highcharts);
