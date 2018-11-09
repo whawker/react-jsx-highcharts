004cadb59dbf1247350949665e8f6c0326e683e6
Antti Lehto <anajavi@users.noreply.github.com>
2018-11-10 00:43:16 +0200

    Add integration test for Axis afterInit event

---
+++ packages/react-jsx-highcharts/test/components/Axis/Axis.integration.spec.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Highcharts from 'Highcharts';
+import {
+  HighchartsChart,
+  withHighcharts
+} from '../../../src';
+import Axis from '../../../src/components/Axis';
+import { renderIntoDocument } from 'react-dom/test-utils'
+
+describe('<Axis />', () => {
+  let testContext;
+
+  beforeEach(() => {
+    testContext = {};
+  });
+
+
+  describe('when rendered to document', () => {
+    it('fires afterInit event', (done) => {
+      const onAfterInit = (event) => {
+        expect(true).toBe(true);
+        done();
+      }
+      const Component = (props) => {
+        return (
+          <HighchartsChart>
+            <Axis onAfterInit={onAfterInit}/>
+          </HighchartsChart>
+        )
+      };
+      const WithComponent = withHighcharts(Component, Highcharts);
+      const renderedChart = renderIntoDocument(<WithComponent />);
+    });
+  });
+});
