99d30e318ba8ba9fd405efac40f72f5eec667065
Jørund Vier Skriubakken <joerund.vier.skriubakken@esmartsystems.com>
2018-11-09 15:30:37 +0100

    highcharts-gantt: Added XRangeSeries - a series of type xrange

--- packages/react-jsx-highcharts-gantt/src/index.js
+++ packages/react-jsx-highcharts-gantt/src/index.js
@@ -23,3 +23,4 @@ export { default as Pathfinder } from './components/Pathfinder';
 // Series
 const parentAxisId = { axisId: 'yAxis' };
 export const GanttSeries = withSeriesType('gantt', parentAxisId);
+export const XRangeSeries = withSeriesType('xrange', parentAxisId);
