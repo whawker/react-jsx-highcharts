df737bc2287d1e9088026a14e38601cd1a6ed129
Antti Lehto <anajavi@users.noreply.github.com>
2018-11-14 14:36:07 +0200

    Add tests for warnings

---
+++ packages/react-jsx-highcharts/test/utils/warnings.spec.js
@@ -0,0 +1,38 @@
+import {
+  logSeriesErrorMessage,
+  logZAxisErrorMessage
+} from "../../src/utils/warnings";
+
+
+describe('utils/warnings', () => {
+  process.env.NODE_ENV = 'development'
+  jest.spyOn(console, "log").mockImplementation(() => {});
+  jest.spyOn(console, "warn").mockImplementation(() => {});
+  jest.spyOn(console, "group").mockImplementation(() => {});
+
+  beforeEach(() => {
+    console.log.mockClear();
+    console.warn.mockClear();
+    console.group.mockClear();
+  });
+
+  describe('logSeriesErrorMessage', () => {
+    it('logs message to console.warn', () => {
+      logSeriesErrorMessage('heatmap');
+
+      expect(console.warn).toHaveBeenCalledWith(
+        expect.stringContaining('This series type "heatmap" requires an additional Highcharts module, or is invalid.')
+      );
+    });
+  });
+  describe('logZAxisErrorMessage', () => {
+    it('logs message to console.log', () => {
+      logZAxisErrorMessage();
+
+      expect(console.log).toHaveBeenCalledWith(
+        expect.stringContaining('This axis type "ZAxis" requires an additional Highcharts module'),
+        expect.anything()
+      );
+    });
+  });
+});
