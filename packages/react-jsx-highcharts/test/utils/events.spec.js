a872c86f767ef1be36b6acdbdc6f79a9240ca29f
Antti Lehto <anajavi@users.noreply.github.com>
2018-11-15 15:52:31 +0200

    Add test for addEventHandlersManually

--- packages/react-jsx-highcharts/test/utils/events.spec.js
+++ packages/react-jsx-highcharts/test/utils/events.spec.js
@@ -1,6 +1,12 @@
 import * as events from '../../src/utils/events';
+import { Highcharts } from '../test-utils';
 
 describe('utils/events', () => {
+
+  beforeEach(() => {
+    Highcharts.addEvent.mockClear();
+  });
+
   describe('getEventHandlerProps', () => {
     const { getEventHandlerProps } = events;
 
@@ -93,6 +99,57 @@ describe('utils/events', () => {
           otherEventHandler: onOtherEventHandler
         }
       }, true);
+    });
+  });
+
+  describe('addEventHandlersManually', () => {
+    const { addEventHandlersManually } = events;
+
+    it('should call the Highcharts.addEvent for passed event handlers', () => {
+      const onEventHandler = jest.fn();
+      const onOtherEventHandler = jest.fn();
+
+      const config = {
+        enabled: true,
+        onEventHandler,
+        onOtherEventHandler,
+        onNotAFunction: 'trip',
+        something: 'stringy',
+        count: 14
+      }
+      const context = {};
+
+      addEventHandlersManually(Highcharts, context, config)
+
+      expect(Highcharts.addEvent).toHaveBeenCalledWith(
+        context, 'eventHandler', onEventHandler
+      );
+      expect(Highcharts.addEvent).toHaveBeenCalledWith(
+        context, 'otherEventHandler', onOtherEventHandler
+      );
+      expect(Highcharts.addEvent).not.toHaveBeenCalledWith(
+        context, 'onNotAFunction', 'trip'
+      );
+    });
+    it('should not call the Highcharts.addEvent when props is undefined', () => {
+      const onEventHandler = jest.fn();
+      const onOtherEventHandler = jest.fn();
+
+      const context = {};
+
+      addEventHandlersManually(Highcharts, context, undefined)
+
+      expect(Highcharts.addEvent).not.toHaveBeenCalled();
+    });
+    it('should not call the Highcharts.addEvent when props is null', () => {
+      const onEventHandler = jest.fn();
+      const onOtherEventHandler = jest.fn();
+
+      const context = {};
+
+      addEventHandlersManually(Highcharts, context, null)
+
+      expect(Highcharts.addEvent).not.toHaveBeenCalled();
     });
   });
 });
