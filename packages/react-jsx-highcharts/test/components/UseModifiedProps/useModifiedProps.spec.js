c62ab7fa7e9c16b13a51032ced7c510bcc746cfe
Antti Lehto <anajavi@users.noreply.github.com>
2019-09-22 22:10:33 +0300

    Add useModifiedProps spec

---
+++ packages/react-jsx-highcharts/test/components/UseModifiedProps/useModifiedProps.spec.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import useModifiedProps from '../../../src/components/UseModifiedProps';
+
+describe('useChartUpdate', () => {
+  let Component;
+  let callback;
+
+  beforeEach(() => {
+    callback = jest.fn();
+    Component = props => {
+      const modifiedProps = useModifiedProps(props);
+      callback(modifiedProps);
+      return null;
+    }
+  });
+
+  it('should return all props on initial mount', () => {
+    const wrapper = mount(<Component someProp={true}/>);
+    expect(callback).toHaveBeenCalledWith({ someProp: true });
+  });
+
+  it('should return changed props', () => {
+    const wrapper = mount(<Component someProp={false} otherProp={false} />);
+    callback.mockClear();
+    wrapper.setProps({ someProp: true });
+    expect(callback).toHaveBeenCalledWith({ someProp: true });
+  });
+
+  it('should return false for not changed props', () => {
+    const wrapper = mount(<Component someProp={true} />);
+    callback.mockClear();
+    wrapper.setProps({});
+    expect(callback).toHaveBeenCalledWith(false);
+  });
+
+  it('should return false for empty props', () => {
+    const wrapper = mount(<Component />);
+    expect(callback).toHaveBeenCalledWith(false);
+  });
+});
