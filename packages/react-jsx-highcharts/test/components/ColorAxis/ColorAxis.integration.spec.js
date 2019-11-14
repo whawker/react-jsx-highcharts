c45f41f1134e127e58b7e51614738c5ba300c4be
Antti Lehto <anajavi@users.noreply.github.com>
2019-11-14 19:31:29 +0200

    Add update and unmount tests for ColorAxis

--- packages/react-jsx-highcharts/test/components/ColorAxis/ColorAxis.integration.spec.js
+++ packages/react-jsx-highcharts/test/components/ColorAxis/ColorAxis.integration.spec.js
@@ -8,42 +8,18 @@ import Series from '../../../src/components/Series';
 import YAxis from '../../../src/components/YAxis';
 import XAxis from '../../../src/components/XAxis';
 import Debug from '../../../src/components/Debug';
-import { render } from 'react-dom';
 
 addColorAxis(Highcharts);
 
 describe('<ColorAxis /> integration', () => {
-  let container;
-
-  beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-  });
-
-  afterEach(() => {
-    document.body.removeChild(container);
-    container = null;
-  });
-
   describe('when mounted', () => {
-    it('renders series with correct coloraxis', done => {
-      const onAfterAddSeries = event => {
-        const colorAxis = event.target.colorAxis.find(
-          c => c.options.id === 'testcoloraxis'
-        );
-        expect(colorAxis).toBeDefined();
-        expect(colorAxis.options.min).toEqual(0);
-        expect(colorAxis.options.max).toEqual(10);
-
-        const series = event.target.series[0];
-        expect(series.colorAxis.options.id).toEqual('testcoloraxis');
-        done();
-      };
+    it('renders series with correct coloraxis', () => {
       const data = [{ y: 1, colorValue: 0 }, { y: 10, colorValue: 10 }];
       const Component = props => {
         return (
           <HighchartsChart>
-            <Chart onAfterAddSeries={onAfterAddSeries} />
+            <Debug />
+            <Chart />
             <XAxis />
             <ColorAxis min={10} max={100} />
             <ColorAxis id="testcoloraxis" min={0} max={10}>
@@ -56,7 +32,79 @@ describe('<ColorAxis /> integration', () => {
         );
       };
       const WithComponent = withHighcharts(Component, Highcharts);
-      render(<WithComponent />, container);
+      mount(<WithComponent />);
+      const chart = window.chart;
+      const colorAxis = chart.colorAxis.find(
+        c => c.options.id === 'testcoloraxis'
+      );
+      expect(colorAxis).toBeDefined();
+      expect(colorAxis.options.min).toEqual(0);
+      expect(colorAxis.options.max).toEqual(10);
+
+      const series = chart.series[0];
+      expect(series.colorAxis.options.id).toEqual('testcoloraxis');
+    });
+  });
+
+  describe('when updated', () => {
+    it('updates colorAxis options', () => {
+      const data = [{ y: 1, colorValue: 0 }, { y: 10, colorValue: 10 }];
+      const Component = ({ min, max }) => {
+        return (
+          <HighchartsChart>
+            <Debug />
+            <Chart />
+            <XAxis />
+            <ColorAxis id="testcoloraxis" min={min} max={max}>
+              <YAxis>
+                <Series colorKey="colorValue" type="column" data={data} />
+              </YAxis>
+            </ColorAxis>
+          </HighchartsChart>
+        );
+      };
+      const WithComponent = withHighcharts(Component, Highcharts);
+      const wrapper = mount(<WithComponent min={0} max={10} />);
+      wrapper.setProps({ min: 1 });
+      const chart = window.chart;
+      const colorAxis = chart.colorAxis.find(
+        c => c.options.id === 'testcoloraxis'
+      );
+      expect(colorAxis.options.min).toEqual(1);
+    });
+  });
+
+  describe('when unmounted', () => {
+    it('removes colorAxis from chart', () => {
+      const data = [{ y: 1, colorValue: 0 }, { y: 10, colorValue: 10 }];
+      const Component = ({ mountColorAxis = true }) => {
+        return (
+          <HighchartsChart>
+            <Debug />
+            <Chart />
+            <XAxis />
+            {mountColorAxis ? (
+              <ColorAxis id="testcoloraxis" min={0} max={10}></ColorAxis>
+            ) : null}
+            <YAxis>
+              <Series
+                colorAxisId="testcoloraxis"
+                colorKey="colorValue"
+                type="column"
+                data={data}
+              />
+            </YAxis>
+          </HighchartsChart>
+        );
+      };
+      const WithComponent = withHighcharts(Component, Highcharts);
+      const wrapper = mount(<WithComponent min={0} max={10} />);
+      wrapper.setProps({ mountColorAxis: false });
+      const chart = window.chart;
+      const colorAxis = chart.colorAxis.find(
+        c => c.options.id === 'testcoloraxis'
+      );
+      expect(colorAxis).not.toBeDefined();
     });
   });
 });
