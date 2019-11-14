7df90fc875c88b4ec664debdbf909d5e3d5ea6f0
Antti Lehto <anajavi@users.noreply.github.com>
2019-11-14 22:57:26 +0200

    Revert "Simplify Navigator"
    This reverts commit 18362b5f49962e72ef93d64196f33ba0e468d089.

--- packages/react-jsx-highstock/src/components/Navigator/Navigator.js
+++ packages/react-jsx-highstock/src/components/Navigator/Navigator.js
@@ -1,14 +1,27 @@
-import React, { useEffect } from 'react';
+import React, {
+  useState,
+  useEffect,
+  Children,
+  cloneElement,
+  isValidElement
+} from 'react';
 import PropTypes from 'prop-types';
 import { attempt } from 'lodash-es';
 import { useModifiedProps, useChart } from 'react-jsx-highcharts';
 import NavigatorXAxis from './NavigatorXAxis';
 
-const Navigator = ({ children = null, enabled = true, ...restProps }) => {
+const Navigator = ({ enabled = true, ...restProps }) => {
   const props = { enabled, ...restProps };
+  const [rendered, setRendered] = useState(false);
   const chart = useChart();
 
   useEffect(() => {
+    const { children, ...rest } = props;
+
+    updateNavigator(rest, chart);
+
+    setRendered(true);
+
     return () => {
       attempt(updateNavigator, { enabled: false }, chart);
     };
@@ -16,13 +29,21 @@ const Navigator = ({ children = null, enabled = true, ...restProps }) => {
 
   const modifiedProps = useModifiedProps(props);
 
-  if (modifiedProps !== false) {
-    updateNavigator(modifiedProps, chart);
-  }
+  useEffect(() => {
+    if (modifiedProps !== false) {
+      updateNavigator(modifiedProps, chart);
+    }
+  });
 
-  if (!children || !enabled) return null;
+  const { children } = props;
+  if (!children || !rendered) return null;
 
-  return <NavigatorXAxis>{children}</NavigatorXAxis>;
+  const navChildren = Children.map(children, child => {
+    if (isValidElement(child) === false) return child;
+    return cloneElement(child, { rendered });
+  });
+
+  return <NavigatorXAxis>{navChildren}</NavigatorXAxis>;
 };
 
 const updateNavigator = (config, chart) => {
@@ -30,8 +51,7 @@ const updateNavigator = (config, chart) => {
 };
 
 Navigator.propTypes = {
-  enabled: PropTypes.bool,
-  children: PropTypes.node
+  enabled: PropTypes.bool
 };
 
 export default Navigator;
