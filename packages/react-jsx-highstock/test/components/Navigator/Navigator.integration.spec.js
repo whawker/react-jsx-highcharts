372b34ebe410d29009a5cdc3264327699c3aa9c3
Antti Lehto <anajavi@users.noreply.github.com>
2019-11-14 22:59:28 +0200

    Add navigator integration tests

---
+++ packages/react-jsx-highstock/test/components/Navigator/Navigator.integration.spec.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import Highstock from 'highcharts/highstock';
+import {
+  Chart,
+  Debug,
+  XAxis,
+  YAxis,
+  LineSeries,
+  withHighcharts
+} from 'react-jsx-highcharts';
+import { HighchartsStockChart, Navigator } from '../../../src';
+
+describe('<Navigator /> integration', () => {
+  describe('when mounted', () => {
+    it('creates navigator on chart', () => {
+      const data = [1, 2, 3, 4, 5];
+      const Component = props => {
+        return (
+          <HighchartsStockChart>
+            <Debug />
+            <Chart />
+            <XAxis />
+            <YAxis>
+              <LineSeries data={data} />
+            </YAxis>
+            <Navigator />
+          </HighchartsStockChart>
+        );
+      };
+      const WithComponent = withHighcharts(Component, Highstock);
+      mount(<WithComponent />);
+      const chart = window.chart;
+
+      expect(chart.options.navigator.enabled).toEqual(true);
+    });
+
+    it('passed additional props to navigator on chart', () => {
+      const data = [1, 2, 3, 4, 5];
+      const Component = props => {
+        return (
+          <HighchartsStockChart>
+            <Debug />
+            <Chart />
+            <XAxis />
+            <YAxis>
+              <LineSeries data={data} />
+            </YAxis>
+            <Navigator height={100} />
+          </HighchartsStockChart>
+        );
+      };
+      const WithComponent = withHighcharts(Component, Highstock);
+      mount(<WithComponent />);
+      const chart = window.chart;
+
+      expect(chart.options.navigator.height).toEqual(100);
+    });
+  });
+
+  describe('when updated', () => {
+    it('changes height of the chart', () => {
+      const data = [1, 2, 3, 4, 5];
+      const Component = props => {
+        return (
+          <HighchartsStockChart>
+            <Debug />
+            <Chart />
+            <XAxis />
+            <YAxis>
+              <LineSeries data={data} />
+            </YAxis>
+            <Navigator {...props} />
+          </HighchartsStockChart>
+        );
+      };
+
+      const WithComponent = withHighcharts(Component, Highstock);
+      const wrapper = mount(<WithComponent />);
+      wrapper.setProps({ height: 110 });
+      const chart = window.chart;
+      expect(chart.options.navigator.height).toEqual(110);
+    });
+  });
+});
