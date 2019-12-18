4009d9b9d73e980930f9185bf33f5a5b336d5e0f
Antti Lehto <anajavi@users.noreply.github.com>
2019-12-18 10:16:05 +0200

    Add AxisSpy

--- packages/react-jsx-highcharts/test/components/PlotBandLine/PlotBandLineLabel.integration.spec.js
+++ packages/react-jsx-highcharts/test/components/PlotBandLine/PlotBandLineLabel.integration.spec.js
@@ -5,18 +5,26 @@ import {
   Debug,
   HighchartsProvider,
   PlotBand,
-  YAxis
+  YAxis,
+  useAxis
 } from '../../../src';
 
 describe('<PlotBandLineLabel /> integration', () => {
+  const AxisSpy = ({ axisId, axisRef }) => {
+    const axis = useAxis(axisId);
+    axisRef.current = axis;
+    return null;
+  };
+
   describe('when parent axis is updated', () => {
     it('updates plotband style', () => {
+      const axisRef = { current: null };
       const Component = ({ labelStyle, yaxisLabels }) => {
         return (
           <HighchartsProvider Highcharts={Highcharts}>
             <HighchartsChart>
-              <Debug />
               <YAxis id="testyaxis" labels={yaxisLabels}>
+                <AxisSpy axisRef={axisRef} />
                 <PlotBand from={1} to={2}>
                   <PlotBand.Label style={labelStyle} />
                 </PlotBand>
@@ -30,8 +38,7 @@ describe('<PlotBandLineLabel /> integration', () => {
         <Component labelStyle={{ color: '#aaa' }} yaxisLabels={{}} />
       );
 
-      const chart = window.chart;
-      const yaxis = chart.get('testyaxis');
+      let yaxis = axisRef.current && axisRef.current.object;
 
       wrapper.setProps({
         labelStyle: { color: '#bbb' },
