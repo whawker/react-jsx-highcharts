3f6fb7533634242563fbbb0890800ae69b7e24ab
Antti Lehto <anajavi@users.noreply.github.com>
2020-02-28 09:30:27 +0200

    Format babel configs

--- packages/react-jsx-highmaps/babel.config.js
+++ packages/react-jsx-highmaps/babel.config.js
@@ -1,17 +1,20 @@
 /* eslint-env node */
-let ENV = process.env.BABEL_ENV || process.env.NODE_ENV || 'development';
+const ENV = process.env.BABEL_ENV || process.env.NODE_ENV || 'development';
 let config = {
-  "plugins": [
-    "@babel/plugin-proposal-class-properties",
-    ["@babel/transform-runtime", { "useESModules": ENV === 'test'? false : true }]
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    [
+      '@babel/transform-runtime',
+      { useESModules: ENV === 'test' ? false : true }
+    ]
   ],
-  "presets": [
-    ["@babel/env", { "modules": ENV === 'test'? "commonjs" : false }],
-    "@babel/react"
+  presets: [
+    ['@babel/env', { modules: ENV === 'test' ? 'commonjs' : false }],
+    '@babel/react'
   ]
 };
 if (ENV !== 'test') {
-  config.plugins.push("transform-react-remove-prop-types");
+  config.plugins.push('transform-react-remove-prop-types');
 }
 
 module.exports = config;
