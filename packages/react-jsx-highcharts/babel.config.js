530d5a0fdf4e903a2b11f022555ef18c35e4b596
Antti Lehto <anajavi@users.noreply.github.com>
2020-02-28 09:37:12 +0200

    Set babel runtime version to make smaller bundle

--- packages/react-jsx-highcharts/babel.config.js
+++ packages/react-jsx-highcharts/babel.config.js
@@ -1,11 +1,14 @@
 /* eslint-env node */
 const ENV = process.env.BABEL_ENV || process.env.NODE_ENV || 'development';
+const pkg = require('./package.json');
+const runtimeVersion = pkg.dependencies['@babel/runtime'];
+
 let config = {
   plugins: [
     '@babel/plugin-proposal-class-properties',
     [
       '@babel/transform-runtime',
-      { useESModules: ENV === 'test' ? false : true }
+      { version: runtimeVersion, useESModules: ENV === 'test' ? false : true }
     ]
   ],
   presets: [
