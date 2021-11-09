380d5d1f9c7e9c930ff56249fa28074f42039922
Antti Lehto <anajavi@users.noreply.github.com>
2021-11-09 09:21:07 +0200

    Add browser bugfixes to babel configuration

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,10 @@ const ENV = process.env.BABEL_ENV || process.env.NODE_ENV || 'development';
 let config = {
   plugins: [],
   presets: [
-    ['@babel/env', { modules: ENV === 'test' ? 'commonjs' : false }],
+    [
+      '@babel/env',
+      { modules: ENV === 'test' ? 'commonjs' : false, bugfixes: true }
+    ],
     '@babel/react'
   ]
 };
