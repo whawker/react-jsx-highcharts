5c3765e62965025eaf42eb8f38d05dc26ac7f775
anajavi <anajavi@users.noreply.github.com>
2022-04-04 10:43:23 +0300

    Allow axis events to be updated. fixes #308 (#333)

--- packages/react-jsx-highcharts/test/components/Axis/Axis.integration.spec.js
+++ packages/react-jsx-highcharts/test/components/Axis/Axis.integration.spec.js
@@ -4,6 +4,7 @@ import { render } from '@testing-library/react';
 
 import { HighchartsChart, HighchartsProvider } from '../../../src';
 import Axis from '../../../src/components/Axis';
+import ContextSpy from '../../ContextSpy';
 
 describe('<Axis /> integration', () => {
   describe('when rendered to document', () => {
@@ -23,6 +24,41 @@ describe('<Axis /> integration', () => {
       };
 
       render(<Component />);
+    });
+  });
+  describe('when updated', () => {
+    it('updates eventhandlers', () => {
+      const axisRef = {};
+
+      const Component = ({ axisProps }) => {
+        return (
+          <HighchartsProvider Highcharts={Highcharts}>
+            <HighchartsChart>
+              <Axis {...axisProps}>
+                <ContextSpy axisRef={axisRef} />
+              </Axis>
+            </HighchartsChart>
+          </HighchartsProvider>
+        );
+      };
+
+      const { rerender } = render(<Component />);
+
+      const onSetExtremes = jest.fn();
+
+      rerender(<Component axisProps={{ onSetExtremes }} />);
+
+      const onSetExtremes2 = jest.fn();
+      axisRef.current.setExtremes(10, 20);
+      expect(onSetExtremes).toHaveBeenCalledTimes(1);
+      onSetExtremes.mockClear();
+
+      rerender(<Component axisProps={{ onSetExtremes: onSetExtremes2 }} />);
+
+      axisRef.current.setExtremes(100, 200);
+      expect(onSetExtremes).not.toHaveBeenCalled();
+
+      expect(onSetExtremes2).toHaveBeenCalledTimes(1);
     });
   });
 });
