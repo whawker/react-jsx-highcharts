bba030761bf037b25ebb9c47c195c34138dbede5
Antti Lehto <anajavi@users.noreply.github.com>
2022-04-27 18:30:06 +0300

    Upgrade dependencies

--- packages/react-jsx-highcharts/jest.config.js
+++ packages/react-jsx-highcharts/jest.config.js
@@ -6,7 +6,12 @@
 const config = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/test/test-helper.js'],
-  testMatch: ['**/test/**/*.spec.js?(x)']
+  testMatch: ['**/test/**/*.spec.js?(x)'],
+  moduleNameMapper: {
+    // This forces Jest/jest-environment-jsdom to use a Node+CommonJS version of uuid, not a Browser+ESM one
+    // See https://github.com/uuidjs/uuid/pull/616
+    '^uuid$': require.resolve('uuid')
+  }
 };
 
 module.exports = config;
