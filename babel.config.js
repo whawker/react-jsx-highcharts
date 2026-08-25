bc925cd92e6b2eda29779ab7411c363336652010
Antti Lehto <anajavi@users.noreply.github.com>
2026-08-25 15:15:15 +0300

    Upgrade babel to v8

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ const ENV = process.env.BABEL_ENV || process.env.NODE_ENV || 'development';
 let config = {
   plugins: [],
   presets: [
-    ['@babel/env', { modules: false, bugfixes: true }],
+    ['@babel/env', { modules: false }],
     ['@babel/react', { runtime: ENV === 'es' ? 'automatic' : 'classic' }]
   ]
 };
