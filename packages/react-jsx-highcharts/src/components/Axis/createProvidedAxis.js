b5cd23d46966863ad40fb899a8de25df395cbe00
Antti Lehto <anajavi@users.noreply.github.com>
2026-09-20 20:17:40 +0300

    Fix crash with highcharts v13.1 on react-jsx-highcharts v5

--- packages/react-jsx-highcharts/src/components/Axis/createProvidedAxis.js
+++ packages/react-jsx-highcharts/src/components/Axis/createProvidedAxis.js
@@ -7,8 +7,25 @@ const createProvidedAxis = axis => {
     type: axis.coll,
     update: axis.update.bind(axis),
     remove: axis.remove.bind(axis),
-    addPlotBandOrLine: axis.addPlotBandOrLine.bind(axis),
-    removePlotBandOrLine: axis.removePlotBandOrLine.bind(axis),
+    addPlotBandOrLine: axis.addPlotBandOrLine
+      ? axis.addPlotBandOrLine.bind(axis)
+      : (opts, coll) => {
+          if (coll === 'plotBands') {
+            const addPlotBand = axis.addPlotBand.bind(axis);
+            addPlotBand(opts);
+          } else if (coll === 'plotLines') {
+            const addPlotLine = axis.addPlotLine.bind(axis);
+            addPlotLine(opts);
+          }
+        },
+    removePlotBandOrLine: axis.removePlotBandOrLine
+      ? axis.removePlotBandOrLine.bind(axis)
+      : id => {
+          const removePlotBand = axis.removePlotBand.bind(axis);
+          removePlotBand(id);
+          const removePlotLine = axis.removePlotLine.bind(axis);
+          removePlotLine(id);
+        },
     getExtremes: axis.getExtremes.bind(axis),
     setExtremes: axis.setExtremes.bind(axis),
     setTitle: axis.setTitle.bind(axis)
